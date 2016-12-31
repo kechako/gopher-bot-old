39008c569d27080d66d805ae6dc789ff19d951cf
Tomato Ketchup <r@554.jp>
2016-12-31 21:12:57 +0900

    Add disturbing plugin

--- cmd/gopher-bot/main.go
+++ cmd/gopher-bot/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kechako/gopher-bot"
 	"github.com/kechako/gopher-bot/plugins/akari"
+	"github.com/kechako/gopher-bot/plugins/disturbing"
 	"github.com/kechako/gopher-bot/plugins/ic"
 	"github.com/kechako/gopher-bot/plugins/iyagoza"
 	"github.com/kechako/gopher-bot/plugins/lgtm"
@@ -48,6 +49,7 @@ func main() {
 	bot.AddPlugin(suddendeath.NewPlugin())
 	bot.AddPlugin(akari.NewPlugin())
 	bot.AddPlugin(ic.NewPlugin())
+	bot.AddPlugin(disturbing.NewPlugin())
 	bot.AddPlugin(zundoko.NewPlugin())
 	bot.AddPlugin(ppap.NewPlugin())
 	bot.AddPlugin(lgtm.NewPlugin())
---
+++ plugins/disturbing/disturbing.go
@@ -0,0 +1,44 @@
+package disturbing
+
+import (
+	bot "github.com/kechako/gopher-bot"
+	"github.com/kechako/gopher-bot/utils"
+)
+
+var keywords = []string{
+	"ヤバイ",
+	"やばい",
+	"やべぇ",
+	"やべえ",
+	"ヤベェ",
+	"ヤベエ",
+	"マジか",
+	"まじか",
+	"すごい",
+	"凄い",
+	"激しい",
+	"心配",
+}
+
+type plugin struct {
+}
+
+func NewPlugin() bot.Plugin {
+	return &plugin{}
+}
+
+func (p *plugin) DoAction(event bot.EventInfo) bool {
+	if !utils.HasKeywords(event.Text(), keywords...) {
+		return false
+	}
+
+	event.PostMessage("穏やかじゃないわね")
+
+	return true
+}
+
+func (p *plugin) Help() string {
+	return `disturbing:
+	穏やかじゃないわね
+`
+}
