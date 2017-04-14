eb782742c83854d4dca149b23f10838b872a8d8e
Tomato Ketchup <r@554.jp>
2017-04-14 11:54:30 +0900

    japaripark plugin analizes the text with japanese tokenizer

--- plugins/japaripark/japaripark.go
+++ plugins/japaripark/japaripark.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/ikawaha/kagome/tokenizer"
 	bot "github.com/kechako/gopher-bot"
 )
 
@@ -15,37 +16,93 @@ var (
 )
 
 type plugin struct {
+	t tokenizer.Tokenizer
 }
 
+// NewPlugin returns a new plugin.
 func NewPlugin() bot.Plugin {
-	return &plugin{}
+	return &plugin{
+		t: tokenizer.New(),
+	}
 }
 
 func (p *plugin) Hello(info bot.BotInfo) {
 }
 
 func (p *plugin) DoAction(event bot.EventInfo) bool {
-	text := event.Text()
-	i := matchIndex(text)
-	if i < 0 {
+	s, ok := p.getSpeciality(event.Text())
+	if !ok {
 		return false
 	}
 
-	specialized := text[0:i]
-	event.PostMessage(fmt.Sprintf("すごーい！きみは%sが得意なフレンズなんだね！", specialized))
+	event.PostMessage(fmt.Sprintf("すごーい！きみは%sが得意なフレンズなんだね！", s))
 
 	return true
 }
 
-func matchIndex(text string) int {
-	for _, keyword := range specializedKeywords {
-		i := strings.Index(text, keyword)
-		if i > 0 {
-			return i
-		}
+func (p *plugin) getSpeciality(text string) (string, bool) {
+	tokens := p.t.Tokenize(text)
+	if len(tokens) == 0 {
+		return "", false
 	}
 
-	return -1
+	l := len(tokens)
+	words := make([]string, 0, l)
+	hasSpeciality := false
+	for i := 0; i < l; i++ {
+		t := tokens[i]
+
+		if t.Class == tokenizer.DUMMY {
+			continue
+		}
+
+		var next tokenizer.Token
+		if i+1 < l {
+			next = tokens[i+1]
+		}
+
+		if matchKeywordToken(t, next) {
+			hasSpeciality = true
+		} else if matchParticle(t, "が") {
+			var moreNext tokenizer.Token
+			if i+2 < l {
+				moreNext = tokens[i+2]
+			}
+			if matchKeywordToken(next, moreNext) {
+				hasSpeciality = true
+			}
+		}
+		if hasSpeciality {
+			break
+		}
+
+		words = append(words, t.Surface)
+	}
+
+	if !hasSpeciality || len(words) == 0 {
+		return "", false
+	}
+
+	return strings.Join(words, ""), true
+}
+
+func matchParticle(t tokenizer.Token, p string) bool {
+	return t.Surface == p && t.Features()[0] == "助詞"
+}
+
+func matchKeywordToken(t, next tokenizer.Token) bool {
+	// token matches "得意"
+	if t.Surface == "得意" && t.Features()[0] == "名詞" {
+		// next token does not match "先"
+		if next.Surface == "先" && next.Features()[0] == "名詞" {
+			// not "得意"
+			return false
+		}
+
+		return true
+	}
+
+	return false
 }
 
 func (p *plugin) Help() string {
